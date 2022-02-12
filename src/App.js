2a2ce19306b2fcf1502b3b8ea9b00e65d12eba02
Ankit kumar aryan <39740693+ankitaryan62@users.noreply.github.com>
2022-02-12 19:08:49 +0530

    Add JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-
-const App = () => {
+/* const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
     React.createElement(Pet, {
@@ -22,6 +21,19 @@ const App = () => {
       breed: "Wheaten Terrier",
     }),
   ]);
-};
+}; */
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+//using JSX
+const App = () => {
+  return (
+  <div>
+    <h1>Adopt Me !</h1>
+    {/* <Pet name="Luna" animal="Dog" breed="Havanese"/>
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
+    <Pet name="Beam" animal="Dog" breed="Wheaten Terrier"/> */}
+    <SearchParams/>
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
